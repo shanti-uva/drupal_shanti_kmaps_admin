b0353b12389db9aff2dc82857f660ff5347ce287
Edward Garrett <edward@pinedrop.com>
2016-08-02 18:10:50 +0100

    add js to reset search in explorer flyout

--- js/shanti_kmaps_typeahead.js
+++ js/shanti_kmaps_typeahead.js
@@ -11,6 +11,7 @@
             var admin = settings.shanti_kmaps_admin;
 
             $('.kmap-typeahead-picker').once('shanti-kmaps').each(function () {
+                console.log('process');
                 var $typeahead = $('.kmap-search-term', this);
                 var $searchreset = $('button.searchreset', this);
                 $searchreset.click(function() {
