43cf47bf3f06081f1c48d546ef80e87c6794f17a
Edward Garrett <edward@pinedrop.com>
2016-08-04 15:28:30 +0100

    use timeOut to force only one response to focusout

--- js/shanti_kmaps_typeahead.js
+++ js/shanti_kmaps_typeahead.js
@@ -3,37 +3,26 @@
  */
 
 (function ($) {
-
     Drupal.behaviors.shantiKmapsAdminTypeahead = {
-
         attach: function (context, settings) {
-
-            var admin = settings.shanti_kmaps_admin;
-
             $('.kmap-typeahead-picker').once('shanti-kmaps').each(function () {
                 var $picker = $(this);
                 // see http://stackoverflow.com/questions/13980448/jquery-focusout-click-conflict
                 var processing = false;
                 $picker.find('.kmap-search-term').focusout(function(e) {
-                    // var $typeahead = $(e.target);
                     if (!processing) {
                         if ($picker.find($('button.searchreset:hover')).length) {
                             processing = true;
                             window.setTimeout(function () {
-                                console.log('processed');
+                                $(e.target).kmapsTypeahead('setValue', '', false);
                                 processing = false;
                             }, 100);
-                            //$typeahead.kmapsTypeahead('setValue', '', false);
                         }
                     }
                 });
             });
         },
-
         detach: function (context, settings) {
-
         }
-
     };
-
 })(jQuery);
