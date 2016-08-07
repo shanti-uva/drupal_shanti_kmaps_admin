5463d3e0f8bd4cbcc58283da5c1c7f0db5782fd7
Edward Garrett <edward@pinedrop.com>
2016-08-07 11:02:21 +0100

    insert magnify icon for typeahead search (MANU-2632)

--- templates/shanti_kmaps_typeahead.tpl.php
+++ templates/shanti_kmaps_typeahead.tpl.php
@@ -10,5 +10,6 @@
     <input id="<?php print $namespace; ?>-search-term"
            class="kmap-search-term form-control form-text" type="text"
            placeholder="Search <?php print $domain; ?>">
+    <span class="icon shanticon-magnify"></span>
     <button type="button" class="btn searchreset" style="display: none;"><span class="icon"></span></button>
 </div>
