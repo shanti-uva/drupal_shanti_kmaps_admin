e612a3e6172516440da286e9aeeee80ed7dc6dde
Edward Garrett <edward@pinedrop.com>
2016-08-08 22:37:18 +0100

    move focus in/out code to kmaps admin (manu-2718)

--- js/shanti_kmaps_typeahead.js
+++ js/shanti_kmaps_typeahead.js
@@ -7,16 +7,37 @@
         attach: function (context, settings) {
             $('.kmap-typeahead-picker').once('shanti-kmaps').each(function () {
                 var $picker = $(this);
-                var $reset = $picker.find('button.searchreset');
-                // see http://stackoverflow.com/questions/13980448/jquery-focusout-click-conflict
-                $picker.find('.kmap-search-term').focusout(function (e) {
-                    if (!$reset.hasClass('resetting') && $reset.is(':hover')) {
-                        $reset.addClass('resetting');
+                var $xbtn = $picker.find('button.searchreset');
+                var $srch = $(".kmaps-tt-input", this);  // the main search input
+                $srch.data("holder", $srch.attr("placeholder"));
+                // --- focusin - focusout
+                $srch.focusin(function () {
+                    $srch.attr("placeholder", "");
+                    $xbtn.show("fast");
+                });
+                $srch.focusout(function () {
+                    $srch.attr("placeholder", $srch.data("holder"));
+                    $xbtn.hide();
+
+                    // see http://stackoverflow.com/questions/13980448/jquery-focusout-click-conflict
+                    if (!$xbtn.hasClass('resetting') && $xbtn.is(':hover')) {
+                        $xbtn.addClass('resetting');
                         $(e.target).kmapsTypeahead('setValue', '', false);
                         window.setTimeout(function () {
-                            $reset.removeClass('resetting');
-                            $reset.hide();
+                            $xbtn.removeClass('resetting');
+                            $xbtn.hide();
                         }, 300);
+                    }
+
+                    var str = $srch.data("holder"); //"Enter Search...";
+                    var txt = $srch.val();
+
+                    if (str.indexOf(txt) > -1) {
+                        $xbtn.hide();
+                        return true;
+                    } else {
+                        $xbtn.show(100);
+                        return false;
                     }
                 });
             });
